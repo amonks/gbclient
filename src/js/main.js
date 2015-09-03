0a66578f1ee742b2901ffb89694cca7db6d6407a
Andrew Monks <a@monks.co>
2015-09-02 22:58:19 -0500

    log incoming tweets

--- src/js/main.js
+++ src/js/main.js
@@ -7,12 +7,14 @@ var tweeter = new Tweeter('//gifbooth-proxy.herokuapp.com')
 $(function () {
 
   tweeter.getTweets(function (tweets) {
+    console.log('got ' + tweets.length + ' tweets')
     for (let tweet of tweets) {
       tweeter.newTweet(tweet, $('#tweets'))
     }
   })
 
   tweeter.streamTweets(function (tweet) {
+    console.log('got a new tweet')
     tweeter.newTweet(tweet, $('#tweets'))
   })
 })
